fcce9e18935ea756b59aa01f1b9997d24be1f5e7
Fosmanthux <63710409+Fosmanthux@users.noreply.github.com>
2020-04-23 11:17:31 +0200

    Add project

---
+++ IceChimes/Assets/Scripts/BeatsScroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatsScroller : MonoBehaviour
+{
+    public float bpm;
+
+    public bool hasStarted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bpm = bpm / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        } else
+        {
+            transform.position -= new Vector3(0f, 0f, bpm * Time.deltaTime);
+        }
+    }
+}
---
+++ IceChimes/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int finalScore = 0;
+
+    public float Xincrement;
+    public float speed;
+    public float leftBoarder;
+    public float rightBoarder;
+    
+    public float jumpHight = 2f;
+
+    private bool isGrounded;
+
+    private Vector3 pos;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+
+        if (transform.position.y == 0)
+            {
+                isGrounded = true;
+            }
+            else isGrounded = false;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > leftBoarder)
+        {
+            pos = new Vector3(transform.position.x - Xincrement, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < rightBoarder)
+        {
+            pos = new Vector3(transform.position.x + Xincrement, transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < jumpHight)
+        {
+            pos = new Vector3(transform.position.x, transform.position.y + jumpHight, transform.position.z);
+        }    
+    }
+}
