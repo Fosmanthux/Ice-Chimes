927dcf0894b50f080b39f9c097f023a896ffd596
Fosmanthux <63710409+Fosmanthux@users.noreply.github.com>
2020-06-01 08:56:38 +0200

    Bear1.1

---
+++ IceChimes/Assets/Scripts/BearAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearAnim : MonoBehaviour
+{
+    private Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.startPlaying == true)
+        {
+            anim.Play("BearRun");
+        }
+
+    }
+}
